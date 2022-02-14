d1ff90e9fa278cc340b8261a8c13aa5f19f5d048
Joshua Toguri <joshua.toguri@gmail.com>
2022-02-14 12:00:06 -0500

    implemented function to get coordinates from ip

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("FetchMyIP didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  fetchCoordsByIP(ip, (error, coordinates) => {
+    if (error) {
+      console.log("fetchCoords didn't work!" , error);
+      return;
+    }
+  
+    console.log('It worked! Returned coordinates:' , coordinates);
+  });
 });
+
--- iss.js
+++ iss.js
@@ -9,7 +9,10 @@ const request = require('request');
  */
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) return callback(error, null);
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
     if (response.statusCode !== 200) {
       callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
@@ -21,4 +24,35 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
